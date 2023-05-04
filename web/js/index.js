a6f8684bcec54188a394dc9732dd6602a9892a40
ttsky256 <126429814+ttsky256@users.noreply.github.com>
2023-05-04 21:55:31 +0800

    Delete web directory

--- web/js/index.js
+++
@@ -1,9 +0,0 @@
- $(document).ready(function (){
-    $.ajax({
-        url: "load.do",
-        type: "GET",
-        complete: function (){
-            $("#cc").load("../index.jsp .content")
-        }
-    })
-})
